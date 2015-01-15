bec203cb013439a8ced08e0b0ab5f0f852b7f8e8
appusree <pillaiaparna@gmail.com>
2015-01-15 15:54:37 -0800

    RESTFUL Webservice call

--- Appusree/HiveInTown.git/HiveInTown/src/com/hiveTown/dao/UserAccountDAO.java
+++ Appusree/HiveInTown.git/HiveInTown/src/com/hiveTown/dao/UserAccountDAO.java
@@ -6,46 +6,37 @@ import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
 
 import com.hiveTown.Exception.HTWebConsoleException;
 
-public class UserAccountDAO extends SimpleJdbcDaoSupport{
-	
+public class UserAccountDAO extends SimpleJdbcDaoSupport {
 
 	private static String checkEmailIdQuery = "select count(*) from USER_ACCOUNT "
 			+ " where EMAIL_ID=?";
 
+	public boolean getUsersByEmail(String email) throws HTWebConsoleException {
 
-	
-	public int getUsersByEmail(String email) throws HTWebConsoleException
-	{
-		
 		int users = 0;
+		boolean isUserAvailable = false;
 
-		try
-		{
+		try {
 			// security review comment
-			if ((null != email) && (email.trim().length() > 0))
-			{
+			if ((null != email) && (email.trim().length() > 0)) {
 				// executing the query
 				users = getJdbcTemplate().queryForInt(checkEmailIdQuery,
 						new Object[] { email });
 
-				
+				if (users > 0) {
+					isUserAvailable = true;
+
+				}
 			}
-		}
-		catch (DataAccessResourceFailureException e)
-		{
-		throw new HTWebConsoleException("SERVER_NOT_FOUND", e);
-		}
-		catch (DataAccessException e)
-		{
+		} catch (DataAccessResourceFailureException e) {
+			throw new HTWebConsoleException("SERVER_NOT_FOUND", e);
+		} catch (DataAccessException e) {
 			throw new HTWebConsoleException("DB_QUERY_FAILURE", e);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			throw new HTWebConsoleException("DB_EXCEPTION", e);
 		}
 
-		return users;
+		return isUserAvailable;
 	}
-
 
 }
